abf031052c8a83eb665c66178abae2a73f2ef4c6
Jad2087 <2257715@cmontmorency.qc.ca>
2025-10-25 15:14:41 -0400

    ajoute lien gsap et animation gsap, scrolltrigger

--- src/script.js
+++ src/script.js
@@ -6,25 +6,132 @@ createApp({
       isDarkMode: false
     };
   },
+
   mounted() {
     // Vérifie si dark mode est actif dans la session
     if (sessionStorage.getItem('darkMode') === 'true') {
       this.isDarkMode = true;
       document.body.classList.add('dark-mode');
     }
+
+    // Animation nav au chargement 
+    const navliens = document.querySelectorAll('.nav');
+    gsap.from(navliens, {
+      x: -20,
+      opacity: 0,
+      duration: 0.6,
+      ease: 'power3.out', // animation doux
+      stagger: 0.15
+    });
+
+    // Animation header principal
+    const nom = document.querySelector('.nom');
+    const portfolio = document.querySelector('.portfolio');
+    gsap.timeline({ 
+      defaults: { 
+        duration: 1, // Durée des animations 
+        ease: 'power3.out' 
+      } 
+    })
+      .from(nom, { 
+        opacity: 0,  // apparait invisible
+        scale: 0.8   // apparait cm un saut
+      })
+      .from(portfolio, { 
+        opacity: 0, 
+        scale: 0.8 
+      }, '-=0.5'); // cmc 0,5 seconde avant fin d'animation 
+
+    // ScrollTrigger pour À propos
+    gsap.registerPlugin(ScrollTrigger);
+    const aPropos = document.querySelector('.a-propos-content');
+      gsap.from(aPropos.querySelector('.texte'), {
+        scrollTrigger: {
+          trigger: aPropos,
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        },
+        x: -50,
+        opacity: 0,
+        duration: 1,
+        ease: 'power3.out'
+      });
+      gsap.from(aPropos.querySelector('.image-container'), {
+        scrollTrigger: {
+          trigger: aPropos,
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        },
+        x: 50,
+        opacity: 0,
+        duration: 1,
+        ease: 'power3.out',
+        delay: 0.2
+      });
+
+    // ScrollTrigger Compétences un tag après un
+    const skills = document.querySelectorAll('.skill-tag');
+      gsap.from(skills, {
+        scrollTrigger: {
+          trigger: '.skills-tags',
+          start: 'top 80%',
+          toggleActions: 'play none none none'
+        },
+        y: 30,
+        opacity: 0,
+        duration: 0.8,
+        ease: 'power3.out',
+        stagger: 0.1
+      });
+    
+    // Animation Projets au scroll une carte après une
+    const projetsSection = document.querySelector(".grille-projets");
+      gsap.from(".carte-container", {
+        scrollTrigger: {
+          trigger: projetsSection,
+          start: "top 80%",
+          toggleActions: "play none none none"
+        },
+        y: 50,
+        opacity: 0,
+        duration: 0.8,
+        ease: "power3.out",
+        stagger: 0.2
+      });
+
+    // Animation modals 
+    const cartes = document.querySelectorAll('.carte-projet[data-modal]');
+    cartes.forEach(carte => {
+      const modalId = carte.getAttribute('data-modal');
+      const modal = document.getElementById(modalId);
+      const modalContent = modal.querySelector('.modal-content');
+
+      carte.addEventListener('click', () => {
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+
+        gsap.fromTo(modalContent,
+          { opacity: 0, 
+            scale: 0.8 
+          },
+          { opacity: 1, 
+            scale: 1, 
+            duration: 0.5, 
+            ease: 'power3.out' }
+        );
+      });
+    });
   },
+
   methods: {
     toggleDarkMode() {
       this.isDarkMode = !this.isDarkMode;
-
-      // Ajoute ou retire la classe sur le body
       document.body.classList.toggle('dark-mode', this.isDarkMode);
-
-      // Sauvegarde l'état dans sessionStorage
       sessionStorage.setItem('darkMode', this.isDarkMode);
     }
   }
 }).mount('#app');
+
 
 // Ouvrir le modal quand on clique sur une carte de projet
 const cartesprojet = document.querySelectorAll('.carte-projet');
